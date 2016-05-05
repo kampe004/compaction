6fa2146e3a331854f2c505d4e9cae52393b239b2
Leo van Kampenhout <l.vankampenhout@uu.nl>
2016-05-05 18:06:27 +0200

    remove Layer.mass; introduce grain parameters
    Layer.mass was superfluous -> it could be calculated from Layer.dz and Layer.dens
    the new grain parameters are d,s and gs

--- dfdm/src/icecoresite.h
+++ dfdm/src/icecoresite.h
@@ -10,93 +10,95 @@
 namespace Densification { 
 
 struct Layer {
-    double T;
-    double dens;
-    double mass;
-    double dz;
+   double T;    // temperature [K]
+   double dens;  // density [kg/m3]
+   double dz;   // thickness [m]
+   double d;    // dendricity
+   double s;    // sphericity
+   double gs;   // grain size [mm]
 };
 
 class IceCoreSite {
-    /// abstract base class for all ice core classes
+   /// abstract base class for all ice core classes
 
 public:
-    IceCoreSite(Settings& settings);
-    ~IceCoreSite() {}; 
+   IceCoreSite(Settings& settings);
+   ~IceCoreSite() {}; 
 
-    void init();
-    void runTimeStep(long dt);
-    void writeFiles();
+   void init();
+   void runTimeStep(long dt);
+   void writeFiles();
 
-    long getSecondsSinceStart();
-    bool hasReachedDensity(double dens);
-    double totalDepth();
+   long getSecondsSinceStart();
+   bool hasReachedDensity(double dens);
+   double totalDepth();
 
-    // member functions with implementation
-    int gridsize() { return grid.size(); };
-    double maxTemp() {
-       double retval = grid[0].T;
-       for (int i = 1; i < gridsize(); i++){
-          retval = std::max(retval,grid[i].T);
-       }
-       return retval;
-    }
-    double minTemp() {
-       double retval = grid[0].T;
-       for (int i = 1; i < gridsize(); i++){
-          retval = std::min(retval,grid[i].T);
-       }
-       return retval;
-    }
-    double maxDens() {
-       double retval = grid[0].dens;
-       for (int i = 1; i < gridsize(); i++){
-          retval = std::max(retval,grid[i].dens);
-       }
-       return retval;
-    }
+   // member functions with implementation
+   int gridsize() { return grid.size(); };
+   double maxTemp() {
+      double retval = grid[0].T;
+      for (int i = 1; i < gridsize(); i++){
+        retval = std::max(retval,grid[i].T);
+      }
+      return retval;
+   }
+   double minTemp() {
+      double retval = grid[0].T;
+      for (int i = 1; i < gridsize(); i++){
+        retval = std::min(retval,grid[i].T);
+      }
+      return retval;
+   }
+   double maxDens() {
+      double retval = grid[0].dens;
+      for (int i = 1; i < gridsize(); i++){
+        retval = std::max(retval,grid[i].dens);
+      }
+      return retval;
+   }
 
-    // polymorphic functions (should be overridden by derived class)
-    virtual std::string toString();
-    virtual void printIceCoreSummary();
+   // polymorphic functions (should be overridden by derived class)
+   virtual std::string toString();
+   virtual void printIceCoreSummary();
 
 protected:
-    Settings settings;
+   Settings settings;
 
-    void accumulate(long dt);
-    void compact(long dt);
-    void compactLigtenberg2011(long dt);
-    void compactAnderson1976(long dt);
-    void compactBarnola1991(long dt);
-    void compactSpencer2001(long dt);
-    void compactBarnolaSpencer(long dt);
-    void compactHerronLangway(long dt);
-    void heatDiffusion(long dt);
-    double getZ550();
-    double getZ830();
+   void accumulate(long dt);
+   void compact(long dt);
+   void compactLigtenberg2011(long dt);
+   void compactAnderson1976(long dt);
+   void compactBarnola1991(long dt);
+   void compactSpencer2001(long dt);
+   void compactBarnolaSpencer(long dt);
+   void compactHerronLangway(long dt);
+   void heatDiffusion(long dt);
+   double getZ550();
+   double getZ830();
  
-    // pure virtual functions (must be implemented by derived class)
-    virtual double surfaceDensity(long time) =0;
-    virtual double surfaceTemperature(long time) =0;
-    virtual double accumulationRate(long time) =0;
-    virtual double annualIntegratedAccumulation() =0;
-    virtual double annualMeanSurfaceTemperature() =0;
-//    virtual double annualSurfaceDensity() =0;
+   // pure virtual functions (must be implemented by derived class)
+   virtual double surfaceDensity(long time) =0;
+   virtual double surfaceTemperature(long time) =0;
+   virtual double accumulationRate(long time) =0;
+   virtual double annualIntegratedAccumulation() =0;
+   virtual double annualMeanSurfaceTemperature() =0;
+//   virtual double annualSurfaceDensity() =0;
 
 private:
-    bool is_initialized = false;
-    const double ref_height = 0.4; //  		# reference height of layer in meters
-    const double dzmax = 2*ref_height; //  # maximum thickness of layer
-    const double dzmin = 0.8*ref_height; // # minimum thickness of layer
-    const unsigned int initial_layers = 1;
+   bool is_initialized = false;
+   const double ref_height = 0.4; //  		# reference height of layer in meters
+   const double dzmax = 2*ref_height; //  # maximum thickness of layer
+   const double dzmin = 0.8*ref_height; // # minimum thickness of layer
+   const unsigned int initial_layers = 1;
 
-    // layers are ordered bottom [grid.front()] to top [grid.back()]
-    std::vector<Layer> grid;
+   // layers are ordered bottom [grid.front()] to top [grid.back()]
+   std::vector<Layer> grid;
 
-    DensificationMethod dm;
+   DensificationMethod dm;
 
-    double getDepthOfDensity(double dens);
+   double getDepthOfDensity(double dens);
 
-    long current_time = 0; // time in seconds since start of simulation
+   long current_time = 0; // time in seconds since start of simulation
 };
 
 
